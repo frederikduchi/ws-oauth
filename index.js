5f145bbdfe5a452930da5bd6f2811e6f31d01ea5
Frederik Duchi <frederik.duchi@telenet.be>
2023-10-31 18:14:12 +0100

    changed authorize function to get_token with backend call

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
-const wsOauth = (function () {
-    async function authorize(client_id, client_secret, token_url) {
-        // check  if the id, client secret and a url are provided
-        if (client_id && client_secret && token_url) {
-            // check if the querystring contains a parameter with a code
-            // only execute the remainging functions if that code was found
-            const checkCode = new URLSearchParams(window.location.search).get('code');
-            if (checkCode) {
-                // url: 'https://www.strava.com/oauth/token'
-                // create a POST request, passing the client id, client secret and code to the url for the token
-                const response = await fetch(token_url, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({client_id, client_secret, code: checkCode, grant_type: 'authorization_code'})
-                })
-                const token = await response.json();
-                console.log(token);
-
-                // return only the access_token
-                return token.access_token;
-            } else {
-                console.log('no code found in url yet');
-                return;
-            }
+const ws_oAuth = (function () {
+    async function get_token(backend_url) {
+        // check if the querystring contains a parameter with a code
+        // only execute the remainging functions if that code was found
+        const checkCode = new URLSearchParams(window.location.search).get('code');
+        if (checkCode) {
+            // call backend service to get the token
+            const response = await fetch(backend_url);
+            const token = await response.json();
+            return token.access_token;
         } else {
-            console.log('please provide a client id, client secret and a url to get the token')
+            console.log('no code found in url yet');
             return;
         }
     }
@@ -45,9 +29,8 @@ const wsOauth = (function () {
 
 
     return {
-        authorize,
+        get_token,
         call_endpoint
-
     }
 
 })();
