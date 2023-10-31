962e49e6992423a45797753435fe82487cdda160
Frederik Duchi <frederik.duchi@telenet.be>
2023-10-31 06:50:23 +0100

    modified fucntions for more flexibility

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-const wsOauth = (function(){
-    async function getToken(code,client_id, client_secret) {
-        const requestBody = {
-            client_id: client_id,
-            client_secret: client_secret,
-            code: code,
-            grant_type: 'authorization_code'
-        };
+const wsOauth = (function () {
+    async function authorize(client_id, client_secret) {
+        // check  if the id and secret are provided
+        if (client_id && client_secret) {
+            // check if the querystirng contains a parameter with a code
+            // only execute the remainging functions if that code was found
+            const checkCode = new URLSearchParams(window.location.search).get('code');
+            if (checkCode) {
+                const requestBody = {
+                    client_id: client_id,
+                    client_secret: client_secret,
+                    code: code,
+                    grant_type: 'authorization_code'
+                };
 
-        console.log(requestBody);
+                console.log(requestBody);
 
-        const response = await fetch('https://www.strava.com/oauth/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(requestBody)
-        })
-        const token = await response.json();
-        console.log(token);
-        getData(token.access_token);
+                const response = await fetch('https://www.strava.com/oauth/token', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(requestBody)
+                })
+                const token = await response.json();
+                console.log(token);
+                
+                return token.access_token;
+            } else {
+                console.log('no code found in url yet');
+                return;
+            }
+        } else {
+            console.log('please provide a client id and secret')
+            return;
+        }
     }
 
     async function getData(token) {
@@ -31,21 +46,11 @@ const wsOauth = (function(){
         console.log(activities);
     }
 
-    function init(client_id, client_secret){
-        // check  if the id and secret are provided
-        if(client_id && client_secret){
-            // check if the querystirng contains a parameter with a code
-            // only execute the remainging functions if that code was found
-            const checkCode = new URLSearchParams(window.location.search).get('code');
-            if (checkCode) {
-                getToken(checkCode, client_id, client_secret);
-            }
-        }else{ console.log('please provide a client id and secret')}
 
-    }
 
     return {
-        init: init
+        authorize: authorize,
+
     }
 
 })();
