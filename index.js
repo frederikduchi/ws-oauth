826e5d6f3ac148fd0db589341d46159243bd71c5
Frederik Duchi <frederik.duchi@telenet.be>
2023-10-31 05:59:49 +0100

    inital commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const wsOauth = (() =>{
+    async function getToken(code) {
+        const requestBody = {
+            client_id: 114716,
+            client_secret: 'b0f13abdb3152be3d6d61962f54ed02f0adb16f2',
+            code: code,
+            grant_type: 'authorization_code'
+        };
+
+        console.log(requestBody);
+
+        const response = await fetch('https://www.strava.com/oauth/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody)
+        })
+        const token = await response.json();
+        console.log(token);
+        getData(token.access_token);
+    }
+
+    async function getData(token) {
+        const response = await fetch('https://www.strava.com/api/v3/athlete/activities', {
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        })
+        const activities = await response.json();
+        console.log(activities);
+    }
+
+    const init = () => {
+        // check if the querystirng contains a parameter with a code
+        // only execute the remainging functions if that code was found
+        const checkCode = new URLSearchParams(window.location.search).get('code');
+        if (checkCode) {
+            getToken(checkCode);
+        }
+    }
+
+    return {
+        init
+    }
+
+})();
