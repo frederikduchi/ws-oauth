11ef7e2090f288dd52ecb1c4fe4fd17f3a67659c
Frederik Duchi <frederik.duchi@telenet.be>
2023-10-31 16:58:23 +0100

    made functions more general and flexible

--- index.js
+++ index.js
@@ -1,55 +1,51 @@
 const wsOauth = (function () {
-    async function authorize(client_id, client_secret) {
-        // check  if the id and secret are provided
-        if (client_id && client_secret) {
-            // check if the querystirng contains a parameter with a code
+    async function authorize(client_id, client_secret, token_url) {
+        // check  if the id, client secret and a url are provided
+        if (client_id && client_secret && token_url) {
+            // check if the querystring contains a parameter with a code
             // only execute the remainging functions if that code was found
             const checkCode = new URLSearchParams(window.location.search).get('code');
             if (checkCode) {
-                const requestBody = {
-                    client_id: client_id,
-                    client_secret: client_secret,
-                    code: code,
-                    grant_type: 'authorization_code'
-                };
-
-                console.log(requestBody);
-
-                const response = await fetch('https://www.strava.com/oauth/token', {
+                // url: 'https://www.strava.com/oauth/token'
+                // create a POST request, passing the client id, client secret and code to the url for the token
+                const response = await fetch(token_url, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
                     },
-                    body: JSON.stringify(requestBody)
+                    body: JSON.stringify({client_id, client_secret, code, grant_type: 'authorization_code'})
                 })
                 const token = await response.json();
                 console.log(token);
-                
+
+                // return only the access_token
                 return token.access_token;
             } else {
                 console.log('no code found in url yet');
                 return;
             }
         } else {
-            console.log('please provide a client id and secret')
+            console.log('please provide a client id, client secret and a url to get the token')
             return;
         }
     }
 
-    async function getData(token) {
-        const response = await fetch('https://www.strava.com/api/v3/athlete/activities', {
+    async function call_endpoint(endpoint_url, token) {
+        const response = await fetch(endpoint_url, {
             headers: {
                 'Authorization': 'Bearer ' + token
             }
         })
-        const activities = await response.json();
-        console.log(activities);
+        const data = await response.json();
+        console.log(data);
+        return data;
     }
 
 
 
     return {
-        authorize: authorize,
+        authorize,
+        call_endpoint
 
     }
 
